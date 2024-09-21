a0644ce6e03cb72d0f805b29ee372352c3464e42
CubeBeveled <cubebeveled@gmail.com>
2024-09-21 15:39:03 +0200

    Made a script that downloads the modpack files

---
+++ scripts/downloader/index.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+const fs = require("fs")
+
+const versions = [
+  "1.21.1",
+  "1.21",
+  "1.20.4",
+  "1.20.3",
+  "1.20.2",
+  "1.20.1",
+  "1.20"
+]
+
+axios.get("https://api.modrinth.com/v3/user/w6wREnpz/collections")
+  .then(async res => {
+    for (const c of res.data) {
+      if (c.id == "jIKTSUDZ") {
+        console.log("Found collection");
+
+        for (const p of c.projects) {
+          const name = (await axios.get(`https://api.modrinth.com/v3/project/${p}`)).data.name;
+          console.log("Getting versions of", name)
+          const modVersions = await axios.get(`https://api.modrinth.com/v3/project/${p}/version`);
+
+          for (const pv of versions) {
+            console.log("Checking for version", pv)
+            for (const mv of modVersions.data) {
+              if (mv.game_versions.includes(pv) && mv.loaders.includes("fabric")) {
+                const file = mv.files[0];
+                console.log("Downloading", file.filename)
+
+                const jar = await axios.get(file.url, {
+                  responseType: "arraybuffer",  // Receive the file as binary data
+                  headers: {
+                    "Content-Type": "application/java-archive"
+                  }
+                });
+
+                fs.writeFileSync(pv + file.filename, jar.data);
+                break;
+              }
+              
+              console.log()
+            }
+          }
+        }
+      }
+    }
+  })
